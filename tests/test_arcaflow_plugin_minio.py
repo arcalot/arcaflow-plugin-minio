b6cf4c2cdfb41cfc2af9d1c93a5cfb5d51ee9aba
Dustin Black <dblack@redhat.com>
2023-06-30 14:41:41 +0200

    add test to create an object in the bucket

--- tests/test_arcaflow_plugin_minio.py
+++ tests/test_arcaflow_plugin_minio.py
@@ -2,7 +2,21 @@
 
 import unittest
 import minio_plugin
+from minio import Minio
+import io
+from time import sleep
+from multiprocessing.pool import ThreadPool
 from arcaflow_plugin_sdk import plugin
+
+
+def run_minio_server():
+    input = minio_plugin.InputParams(
+        run_duration=5,
+        minio_user="foo",
+        minio_password="bartholomew",
+        bucket_name="mybucket",
+    )
+    return minio_plugin.minio(input)
 
 
 class minioTest(unittest.TestCase):
@@ -13,6 +27,7 @@ class minioTest(unittest.TestCase):
                 run_duration=3,
                 minio_user="foo",
                 minio_password="bartholomew",
+                bucket_name="mybucket",
             )
         )
 
@@ -28,13 +43,33 @@ class minioTest(unittest.TestCase):
         )
 
     def test_functional(self):
-        input = minio_plugin.InputParams(
-            run_duration=3,
-            minio_user="foo",
-            minio_password="bartholomew",
+        pool = ThreadPool(processes=1)
+
+        minio_server = pool.apply_async(run_minio_server)
+
+        client = Minio(
+            "127.0.0.1:9000",
+            access_key="foo",
+            secret_key="bartholomew",
+            secure=False,
         )
 
-        output_id, output_data = minio_plugin.minio(input)
+        while not client.bucket_exists("mybucket"):
+            print("Waiting for bucket ...")
+            sleep(1)
+
+        obj_result = client.put_object(
+            "mybucket",
+            "myobject",
+            io.BytesIO(b"hello"),
+            5,
+        )
+        print(
+            f"Created {obj_result.object_name} object; etag: {obj_result.etag}, "
+            f"version-id: {obj_result.version_id}"
+        )
+
+        output_id, output_data = minio_server.get()
 
         self.assertEqual("success", output_id)
         self.assertEqual("foo", output_data.access_key)
